f2b27de9b14eb0a90cd7b55fdcb58603000e7952
Michel Doumit <mi.doumit@gmail.com>
2016-09-25 19:17:01 -0300

    first remove duplciate version

--- linked-list/remove_duplicates.rb
+++ linked-list/remove_duplicates.rb
@@ -1,12 +1,27 @@
 require_relative '../helpers.rb'
 require_relative 'linked_item.rb'
 
-
-
+# first simple
 def remove_duplicate(head)
+  existing = [head.data]
+  current = head
+  while current.next_item != nil
+    if existing.include?(current.next_item.data)
+      if current.next_item.next_item
+        current.next_item = current.next_item.next_item
+      else
+        current.next_item = nil
+        break
+      end
+    else
+      existing << current.data
+    end
+
+    current = current.next_item
+  end
+
   head
 end
 
-
-head = LinkedItem.from_array([1,2,3,4,5,6,4,8])
-assert(remove_duplicate(head).to_array, [1,2,3,4,5,6,8])
+assert(remove_duplicate(list([1,2,3,4,5,6,4,8])).to_array, [1,2,3,4,5,6,8])
+assert(remove_duplicate(list([1,2,3,4,5,6,7,4])).to_array, [1,2,3,4,5,6,7])
