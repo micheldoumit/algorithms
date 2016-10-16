2087cf921d1387b7910e68061abde9c6a76342c7
Michel Doumit <mi.doumit@gmail.com>
2016-10-16 19:39:24 -0200

    stackmin book

--- stack-queues/stack_min.rb
+++ stack-queues/stack_min.rb
@@ -1,6 +1,7 @@
 require_relative '../helpers'
 require_relative 'stack_node'
 
+# min
 class StackMin
 
   attr_accessor :min, :buffer
@@ -28,6 +29,51 @@ end
 
 
 stacks = StackMin.new
+stacks.push(5)
+stacks.push(7)
+stacks.push(8)
+stacks.push(2)
+stacks.push(9)
+stacks.push(1)
+assert(stacks.min, 1)
+assert(stacks.pop, 1)
+assert(stacks.min, 2)
+assert(stacks.pop, 9)
+assert(stacks.min, 2)
+assert(stacks.pop, 2)
+assert(stacks.min, 5)
+
+
+class StackMin2
+
+  attr_accessor :buffer, :mins
+
+  def initialize
+    self.buffer = []
+    self.mins = []
+  end
+
+  def push(value)
+    min_changed = self.min == nil || value < self.min
+    self.mins << value if min_changed
+    self.buffer << value
+  end
+
+  def pop
+    value = self.buffer.delete_at(self.buffer.length-1)
+    if self.min == value
+      self.mins.delete_at(self.mins.length-1)
+    end
+
+    value
+  end
+
+  def min
+    self.mins.last
+  end
+end
+
+stacks = StackMin2.new
 stacks.push(5)
 stacks.push(7)
 stacks.push(8)
